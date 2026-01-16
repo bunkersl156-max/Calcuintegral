9989698bfdab0305c878e5b6e518d77bd9664c65
bunkersl156-max <bunkersl156@gmail.com>
2026-01-16 19:40:05 +0100

    Fix argument order in nerdamer integration call

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ function calculateIntegral() {
 
             if (a === "" || b === "") throw "Faltan límites";
 
-            result.textContent = nerdamer(`defint(${func}, x, ${a}, ${b})`).toString();
+            result.textContent = nerdamer(`defint(${func}, ${a}, ${b}, x)`).toString();
         }
     } catch {
         result.textContent = "Error en la función o los datos";
